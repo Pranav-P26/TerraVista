aa909b6f1d4c28a56797cc74714580e38a34ef71
Pranav-P26 <pramod.pranav26@gmail.com>
2025-02-01 22:54:00 -0500

    Factored in relative earth sizing

--- ThreeJS_Project/src/App.jsx
+++ ThreeJS_Project/src/App.jsx
@@ -33,15 +33,6 @@ function App() {
     controls.maxPolarAngle = Math.PI;
     controls.update();
 
-    const ambientLight = new THREE.SpotLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
-
     const earth = createEarth(scene);
 
     earth.rotation.x = THREE.MathUtils.degToRad(-EARTH_AXIAL_TILT);
--- ThreeJS_Project/src/Planets/Earth/Earth.js
+++ ThreeJS_Project/src/Planets/Earth/Earth.js
@@ -1,12 +1,17 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 export function createEarth(scene) {
+  const EARTH_RADIUS_KM = 6371;
+  const SCALE_FACTOR = 1000; // Adjusted scale factor to a more reasonable value
+
+  const earthRadius = EARTH_RADIUS_KM / SCALE_FACTOR;
+
   const textureLoader = new THREE.TextureLoader();
-  const texture = textureLoader.load('src/assets/Earth/Earth_Diffuse.jpg');
+  const texture = textureLoader.load("src/assets/Earth/Earth_Diffuse.jpg");
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
 
-  const geometry = new THREE.SphereGeometry(16, 64, 32);
+  const geometry = new THREE.SphereGeometry(earthRadius, 32, 32);
   const material = new THREE.MeshBasicMaterial({ map: texture });
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
